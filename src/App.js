30f8b21f594107b6e3552a4bf6661accfa7fb03a
sostenesapollo <sostenesapollo25@gmail.com>
2020-11-04 17:40:08 -0300

    ✔️ Upper and Contador added

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+function App() {  
+  return (
+    <>      
+      <Upper/>
+      <Contador start={20}/>
+    </>
+  )
+}
+
+function Upper() {
+
+  function UpperCase(props) {
+    return props.text.toUpperCase()
+  }
+
+  function UpperCaseContent(props) {
+    return props.children.toUpperCase()
+  }
+
+  return (
+    <>
+      <h1>My app</h1>
+      <p>
+        <UpperCase text="Texto que ficará em caixa alta"/>    
+      </p>
+      <p>
+        <UpperCaseContent>Conteúdo que está dentro da div</UpperCaseContent>
+      </p>
+    </>
+  )
+
+}
+
+function Contador(props) {
+
+  const [contador, setContador] = useState(props.start ? props.start : 0)
+
+  function IncrementaContador() {
+    setContador(contador+1)
+  }
+
+  return (
+    <>
+      <p>{contador}</p>
+      <button onClick={IncrementaContador}>Incrementar</button>
+    </>
+  )  
+}
+
+export default App;
